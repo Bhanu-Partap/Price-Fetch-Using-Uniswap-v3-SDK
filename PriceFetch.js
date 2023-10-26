471ec10f573329b239d7add2d06bd09ef0ff36a4
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-26 09:56:52 +0530

    Rpc Provider

--- PriceFetch.js
+++ PriceFetch.js
@@ -1,2 +1,4 @@
 const {ethers} = require("ethers")
 const Quoter = require('@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json')
+
+const Provider= new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/vcTD-zoE41OoKyny7NQmsTzsO2wpwFUh")
