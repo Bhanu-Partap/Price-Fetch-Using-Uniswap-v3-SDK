a526596264b290a298b1143d407eeed694dd5fb9
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-26 10:44:41 +0530

    fetch price function

--- PriceFetch.js
+++ PriceFetch.js
@@ -7,5 +7,6 @@ const main = async()=>{
     const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" //WETH
     const addressTo = "0x6B175474E89094C44Da98b954EedeAC495271d0F" //DAI
 
-    
+    const humanValue ="1";
+    const result = await fetchPrice(addressFrom, addressTo, humanValue);
 }
