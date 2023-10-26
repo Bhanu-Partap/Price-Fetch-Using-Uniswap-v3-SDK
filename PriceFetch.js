748c0a7de83def544308219f9e446f50936dec39
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-26 10:47:46 +0530

    fetch price function completed

--- PriceFetch.js
+++ PriceFetch.js
@@ -3,10 +3,22 @@ const Quoter = require('@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.so
 
 const Provider= new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/vcTD-zoE41OoKyny7NQmsTzsO2wpwFUh")
 
+
+const fetchPrice = async(addressFrom, addressTo, humanValue)=>{
+    const quoterContract = new ethers.Contract(
+        QUOTER_CONTRACT_ADDRESS,
+        Quoter.abi,
+        getProvider()
+      )
+}
+
 const main = async()=>{
     const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" //WETH
     const addressTo = "0x6B175474E89094C44Da98b954EedeAC495271d0F" //DAI
 
     const humanValue ="1";
     const result = await fetchPrice(addressFrom, addressTo, humanValue);
+    console.log(result);
 }
+
+main()
